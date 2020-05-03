558332469b0234c4bd90f75b9d821b096230a11a
Shubham Jain <Shubhamjain2908@gmail.com>
2020-05-03 22:07:25 +0530

    creating grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const numRows = 50;
+const numCols = 50;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
   return (
-    <div>
-      yo
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+      {grid.map((rows, i) =>
+        rows.map((col, k) => <div key={`${i} - ${k}`} style={{
+          width: 20,
+          height: 20,
+          backgroundColor: grid[i][k] ? 'pink' : undefined,
+          border: 'solid 1px black'
+        }} />)
+      )}
     </div>
   );
 }
